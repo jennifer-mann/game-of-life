7c538e4a8828a5a47a151c50d1ed47e7fb7fbdcc
Jennifer Mann <jennifer@Jennifers-MacBook-Pro-2.local>
2014-10-26 08:58:29 -0400

    refactored some of the code, experimented with Famo.us setProperties

--- js/main.js
+++ js/main.js
@@ -6,69 +6,77 @@
 
 window.Conway = (function (){
   // import dependencies
-  var Engine = famous.core.Engine;
-  var Modifier = famous.core.Modifier;
-  var Transform = famous.core.Transform;
-  var Surface = famous.core.Surface;
-  var GridLayout = famous.views.GridLayout;
-  var StateModifier = famous.modifiers.StateModifier;
+  var Engine = famous.core.Engine
+  var Modifier = famous.core.Modifier
+  var Transform = famous.core.Transform
+  var Surface = famous.core.Surface
+  var GridLayout = famous.views.GridLayout
+  var StateModifier = famous.modifiers.StateModifier
+  var Timer    = famous.utilities.Timer
 
   // create the main context
-  var mainContext = Engine.createContext();
+  var mainContext = Engine.createContext()
 
-  var rows = 10;
-  var columns = 10;
+  var rows = 10
+  var columns = 10
 
   // create the container to hold the grid
   var containerModifier = new StateModifier({
     origin: [0.5, 0.5],
     align: [0.5, 0.5],
     size: [200, 200]
-  });
+  })
 
   // setup the grid layout
   var grid = new GridLayout({
     size: [100, 100],
     dimensions: [rows, columns]
-  });
+  })
 
-  // var dead = "#354B60";
-  // var alive = "#27ae60";
+  var dead = "#354B60";
+  var alive = "#27ae60";
 
   // var initialColor = dead;
 
   // create surfaces array
-  var surfaces = [];
+  var surfaces = []
 
   // Sets the collection of renderables under the Gridlayout instance's control.
-  grid.sequenceFrom(surfaces);
+  grid.sequenceFrom(surfaces)
 
-  initWorld = function() {
-    for(var i = 0; i < (rows * columns); i++) {
-      var cellName = "cell" + i.toString();
-      var str = "surface" + i.toString();
 
-      str = new Surface({
-        size: [undefined, undefined],
-        classes: [cellName],
-        properties: {
-          border: "1px solid #000"
-        }
-      });
+  for(var i = 0; i < (rows * columns); i++) {
+    var cellName = "cell" + i.toString()
 
-      surfaces.push(str);
-      // console.warn(str.classList[0]);
+    var color = i % 2 ? alive : dead
 
-      if (str.classList[0] === "cell33") {
-        str.addClass("yellow");
+    var str = new Surface({
+      content: i,
+      size: [undefined, undefined],
+      classes: [cellName],
+      properties: {
+        border: "1px solid #000",
+        backgroundColor: color
       }
+    })
 
-      cellName = "";
-    }
+    // var cell = str.classList[0]
+
+    // build glider
+    // if (cell === "cell25" || cell === "cell33" || cell === "cell35" || cell === "cell45" || cell === "cell44") {
+    //   str.addClass("yellow")
+    // }
+
+
+    surfaces.push(str)
   }
 
-  mainContext.add(containerModifier).add(grid);
+  surfaces[99].setProperties({backgroundColor: "red"})
 
-  initWorld();
+  Timer.setInterval(function() {
+      // surface.setContent('this function has run ' + ++counter + ' time(s)')
+  }, 1000);
 
-})();
+  mainContext.add(containerModifier).add(grid)
+
+})()
